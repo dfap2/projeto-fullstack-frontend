0c731e80c839d1dcb8df8f1f619b260ebdb9e58f
David Actis <davidactisp@gmail.com>
2023-10-18 15:37:05 -0300

    feat: main modal

---
+++ src/components/Modal/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledModal = styled.dialog`
+    max-width: 52rem;
+    width: 100%;
+
+    padding: 1.6rem;
+    gap: 3.2rem;
+
+    border-radius: var(--radius-default);
+`;
